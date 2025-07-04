7cc2b6eccc3ee3f33d88beece755d29ca2e6409d
jhantu626 <jhantubala626@gmail.com>
2025-07-04 10:36:32 +0530

    dev: resolved the bug of invoice page fetch data and logic

--- src/Screens/AppScreens/Invoice/Invoice.jsx
+++ src/Screens/AppScreens/Invoice/Invoice.jsx
@@ -7,7 +7,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import Layout from '../../Layout/Layout';
 import {useAuth} from '../../../Context/AuthContext';
 import InvoiceCard from '../../../Components/Cards/InvoiceCard';
@@ -15,10 +15,13 @@ import {
   FloatingAddButton,
   SearchInput,
   SecondaryHeader,
+  ShareBottomSheet,
 } from '../../../Components';
 import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import {invoiceService} from '../../../Services/InvoiceService';
 import Loader from '../../../Components/Loaders/Loader';
+import {RefreshControl} from 'react-native-gesture-handler';
+import {colors} from '../../../utils/colors';
 
 const Invoice = () => {
   // CONTEXT
@@ -30,24 +33,31 @@ const Invoice = () => {
   // STATES
   const [invoices, setInvoices] = useState([]);
   const [pageNo, setPageNo] = useState(0);
+  const [query, setQuery] = useState('');
+  const [sharableInvoices, setSharableInvoices] = useState(null);
+
+  // Ref States
+  const bottomSheetRef = useRef(null);
 
   // LOADING STATE
   const [isLoading, setIsLoading] = useState(true);
   const [hasMore, setHasMore] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
-  const fetchInvoices = async ({reset=false}) => {
+  const fetchInvoices = async ({reset = false}) => {
     try {
-      const currentPage=reset?0:pageNo;
+      setInvoices(prev => (reset ? [] : prev));
+      const currentPage = reset ? 0 : pageNo;
       const data = await invoiceService.getInvoice({
         authToken: authToken,
-        pageNo: pageNo,
+        pageNo: currentPage,
         pageSize: 10,
       });
       if (data.length === 0) {
         setHasMore(false);
       }
-      setInvoices(prev => [...prev, ...data]);
-      if(reset){
+      setInvoices(prev => (pageNo === 0 ? data : [...prev, ...data]));
+      if (reset) {
         setPageNo(0);
       }
     } catch (error) {
@@ -58,39 +68,67 @@ const Invoice = () => {
 
   useFocusEffect(
     useCallback(() => {
-      setInvoices([]);
-      setPageNo(0);
       fetchInvoices({reset: true});
-      console.log('useFocusEffect');
-    }, [authToken]),
+    }, []),
   );
 
-  // useEffect(() => {
-  //   if (pageNo > 0) {
-  //     fetchInvoices();
-  //   }
-  // }, [pageNo]);
+  useEffect(() => {
+    if (pageNo > 0) {
+      fetchInvoices({reset: pageNo === 0});
+    }
+  }, [pageNo]);
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchInvoices({reset: true});
+    setRefreshing(false);
+  };
+
+  const Header = useMemo(
+    () => <SearchInput value={query} setValue={setQuery} />,
+    [query, setQuery],
+  );
+
+  const filteredInvoices = useMemo(() => {
+    if (!query) return invoices;
+
+    const lowerCaseQuery = query.toLowerCase();
+    return invoices.filter(
+      item =>
+        item.customerName?.toLowerCase().includes(lowerCaseQuery) ||
+        item.invoiceNumber?.toLowerCase().includes(lowerCaseQuery),
+    );
+  }, [invoices, query]);
 
   return (
     <Layout>
       <SecondaryHeader navigation="back" title="Invoice" />
       <FlatList
-        ListHeaderComponent={() => <SearchInput />}
+        ListHeaderComponent={Header}
         ListHeaderComponentStyle={{paddingVertical: 15}}
         style={{flex: 1}}
         contentContainerStyle={styles.container}
-        data={invoices}
+        data={filteredInvoices}
         keyExtractor={(item, index) => index + ' key'}
-        renderItem={({item}, index) => <InvoiceCard invoice={item} />}
+        renderItem={({item}, index) => (
+          <InvoiceCard
+            key={index + 'invoice-card'}
+            invoice={item}
+            onPressFunction={() => {
+              setSharableInvoices(index);
+              bottomSheetRef.current?.expand();
+            }}
+          />
+        )}
         showsVerticalScrollIndicator={false}
         onEndReached={() => {
           console.log('end reached');
-          if (hasMore) {
+          if (hasMore && query.length === 0) {
             setPageNo(prev => prev + 1);
           }
         }}
         ListFooterComponent={() =>
-          hasMore ? (
+          hasMore && query.length === 0 ? (
             <Loader />
           ) : (
             <View
@@ -121,11 +159,24 @@ const Invoice = () => {
         ListFooterComponentStyle={{
           paddingVertical: 10,
         }}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={[colors.primary]}
+          />
+        }
       />
       <FloatingAddButton
         onPress={() => {
           navigation.navigate('CreateInvoice');
         }}
+      />
+      <ShareBottomSheet
+        ref={bottomSheetRef}
+        snapPoints={useMemo(() => ['15%'], [])}
+        key={'bottomSheet-share'}
+        invoice={filteredInvoices[sharableInvoices]}
       />
     </Layout>
   );
